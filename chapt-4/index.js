d9ded0c5add5f7c6983424edc3f80677b5adac86
Pokiza Sdv <pokizasdv@gmail.com>
2023-10-27 23:24:00 -0400

    Solve Problem Converting numerals into Romans

--- chapt-4/index.js
+++ chapt-4/index.js
@@ -37,3 +37,67 @@ const shorterString = (arr, length) => {
     return arr.filter((str) => str.length >= length);
 };
 console.log(shorterString(["abc", "defg", "a", "xy"], 3));
+
+// //  Integer to Roman Numerals Given a positive integer that is less than 4000, return a string containing that value in Roman numeral representation. In this representation, I is 1, V is 5, X is 10, L = 50, C = 100, D = 500, and M = 1000. Remember that 4 is IV, 349 is CCCIL and 444 is CDXLIV.
+
+// const intToRoman = (num) => {
+//     let romans = "";
+//     const dic = {
+//       1000: "M",
+//       900: "CM",
+//       500: "D",
+//       400: "CD",
+//       100: "C",
+//       90: "XC",
+//       50: "L",
+//       40: "XL",
+//       10: "X",
+//       9: "IX",
+//       5: "V",
+//       4: "IV",
+//       1: "I"
+//     };
+
+//     for (let key in dic) {
+//       while (num >= key) {
+//         romans += dic[key];
+//         num -= key;
+//       }
+//     }
+
+//     return romans;
+//   };
+const intToRoman = (num) => {
+    const romanNum = [
+        "M",
+        "CM",
+        "D",
+        "CD",
+        "C",
+        "XC",
+        "L",
+        "XL",
+        "X",
+        "IX",
+        "V",
+        "IV",
+        "I"
+    ];
+
+    const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romans = "";
+    for (let i = 0; i < romanNum.length; i++) {
+        while (num >= values[i]) {
+            romans += romanNum[i];
+            num -= values[i];
+        }
+    }
+    return romans;
+};
+
+console.log("_____________");
+console.log(intToRoman(349));
+console.log(intToRoman(4));
+console.log(intToRoman(21));
+console.log(intToRoman(444));
+console.log(intToRoman(1));
