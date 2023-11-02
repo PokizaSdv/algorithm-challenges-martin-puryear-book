61be41e0fc107383f97732ffb2661162a553196c
Pokiza Sdv <pokizasdv@gmail.com>
2023-11-01 22:37:22 -0400

    Solve Problem checking is palindrome string

--- chapt-4/page145-146.js
+++ chapt-4/page145-146.js
@@ -55,3 +55,54 @@ console.log("______________________");
 console.log(isValidSequence("W(a{t}s[o(n{ c}o)m]e)h[e{r}e]!")); // Output: true
 console.log(isValidSequence("D(i{a}l[ t]o)n{e")); // Output: false
 console.log(isValidSequence("A(1)s[O (n]0{t) 0}k")); // Output: false
+
+// ------------------------------------------------
+
+// String: Is Palindrome:
+//  Create a function that returns a boolean whether the string is a strict palindrome. For "a x a" or "racecar", return true. Do not ignore spaces, punctuation and capitalization: if given "Dud" or "oho!", return false.
+
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+console.log("--------------------");
+console.log(isPalindrome("a x a"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("Dud"));
+console.log(isPalindrome("oho!"));
+
+// Second: now do ignore white space (spaces, tabs, returns), capitalization and punctuation.
+
+function isPalindrome2() {
+    let letters = "abcdefghijklmnopqrstuvwxyz"
+    let cleanedStr = "";
+    for (const char of str) {
+        if(letters.includes(char)) {
+            cleanedStr += char
+        }
+    }
+    let left = 0;
+    let right = cleanedStr.length - 1;
+    
+    for (let i = 0; i < Math.floor(cleanedStr.length / 2); i++) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return false;
+}
+console.log("--------------------");
+console.log(isPalindrome("a x a"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("Dud"));
+console.log(isPalindrome("oho!"));
