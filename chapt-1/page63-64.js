0293121152fa624f107f633ba3e94513c875ae52
Pokiza Sdv <pokizasdv@gmail.com>
2023-11-02 23:56:40 -0400

    Solve problem Counting Down

---
+++ chapt-1/page63-64.js
@@ -0,0 +1,11 @@
+// Countdown: Create a function that accepts a number as an input. Return a new array that counts down by one, from the number (as array’s ‘zeroth’ element) down to 0 (as the last element). How long is this array?
+
+const countdown = (number) => {
+    let result = [];
+    while (number >= 0) {
+        result.push(number);
+        number--;
+    }
+    return result;
+};
+console.log(countdown(5));
